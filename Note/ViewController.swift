019f8b96e0afaf801c020b7f237fb54a712a3a34
Seyed Ali Shahrokhi <Alishahrokhi@icloud.com>
2020-04-04 16:07:36 +0430

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Seyed Ali Shahrokhi on 1/16/1399 AP.
+//  Copyright © 1399 Seyed Ali Shahrokhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
